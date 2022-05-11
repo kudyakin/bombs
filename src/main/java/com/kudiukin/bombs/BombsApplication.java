87afbbce6096995894049a96af6ba956f1bb2690
Yevhen Kudiukin <kudyakin@gmail.com>
2022-05-11 12:34:57 +0300

    Initial commit

---
+++ src/main/java/com/kudiukin/bombs/BombsApplication.java
@@ -0,0 +1,13 @@
+package com.kudiukin.bombs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BombsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BombsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kudiukin/bombs/BombsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kudiukin.bombs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BombsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
